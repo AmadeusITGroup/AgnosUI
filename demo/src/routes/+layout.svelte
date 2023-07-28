e31bb694050688377eb0c27e97146d920c07a093
Basso <fabrice.basso@amadeus.com>
2023-07-28 18:30:33 +0200

    demo: firt draft of landing page (#34)
    * demo: firt draft of landing page
    
    * demo: remove id, css and title from Angular, React and Svelte logo files
    
    * fix: centering landing page + constrast
    
    * feat: add warning WIP badge to main nav
    
    ---------
    
    Co-authored-by: David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
    Co-authored-by: Quentin Deroubaix <quentin.deroubaix@amadeus.com>

--- demo/src/routes/+layout.svelte
+++ demo/src/routes/+layout.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-	import {page} from '$app/stores';
-	import {menu} from './navigation';
-	import 'bootstrap/dist/css/bootstrap.css';
-	import './styles.scss';
-	import {pathToRoot$, canonicalURL$} from '../lib/stores';
-	import {selectedFramework$} from '../lib/stores';
 	import Svg from '$lib/layout/Svg.svelte';
 	import github from 'bootstrap-icons/icons/github.svg?raw';
 	import twitter from 'bootstrap-icons/icons/twitter.svg?raw';
+	import 'bootstrap/dist/css/bootstrap.css';
+	import {canonicalURL$, pathToRoot$} from '../lib/stores';
+	import './styles.scss';
 </script>
 
 <svelte:head>
@@ -19,7 +16,8 @@
 		<div class="container-fluid">
 			<a class="navbar-brand" href={$pathToRoot$}> AgnosUI </a>
 			<div class="d-flex align-items-center">
-				<a class="nav-link text-white" href="https://github.com/AmadeusITGroup/AgnosUI" aria-label="link to GitHub repository" target="_blank">
+				<span class="badge bg-warning text-bg-warning">Work In Progress</span>
+				<a class="nav-link text-white ms-3" href="https://github.com/AmadeusITGroup/AgnosUI" aria-label="link to GitHub repository" target="_blank">
 					<Svg className="icon-24 align-middle" svg={github} />
 				</a>
 				<a class="nav-link text-white ms-3" href="https://twitter.com/AgnosUI" aria-label="link to twitter account" target="_blank">
@@ -29,39 +27,9 @@
 			</div>
 		</div>
 	</nav>
-	<div class="demo-main">
+	<div class="demo-main d-flex justify-content-center">
 		<div class="container-xxl">
-			<div class="container-fluid demo-gutter demo-layout">
-				<aside class="demo-sidebar">
-					<nav class="w-100">
-						<ul class="list-unstyled mb-0 pb-3 pb-md-2 pe-lg-2">
-							{#each menu as { title, submenu }}
-								<li class="py-2" id="sidenav-{title}">
-									<strong class="d-flex w-100 align-items-center fw-semibold">{title}</strong>
-									<ul class="list-unstyled fw-normal pb-2 small">
-										{#each submenu as { label, path }}
-											{@const isCurrent = $page.route.id?.startsWith(`/[framework]${path}`)}
-											<li>
-												<a
-													class="demo-links-link d-inline-block rounded"
-													class:active={isCurrent}
-													aria-current={isCurrent ? 'page' : undefined}
-													href="{$pathToRoot$}{$selectedFramework$}{path}"
-												>
-													{label}
-												</a>
-											</li>
-										{/each}
-									</ul>
-								</li>
-							{/each}
-						</ul>
-					</nav>
-				</aside>
-				<div>
-					<slot />
-				</div>
-			</div>
+			<slot />
 		</div>
 	</div>
 </div>
@@ -83,7 +51,6 @@
 	}
 
 	.demo-main {
-		grid-area: main;
 		overflow: auto;
 	}
 </style>
--- demo/src/routes/+page.svelte
+++ demo/src/routes/+page.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	// import {Alert} from '@agnos-ui/svelte';
+	import Svg from '$lib/layout/Svg.svelte';
+	import angularLogo from '../resources/logo-angular.svg?raw';
+	import reactLogo from '../resources/logo-react.svg?raw';
+	import sveteLogo from '../resources/logo-svelte.svg?raw';
 </script>
 
 <svelte:head>
@@ -6,4 +11,43 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>Home page</section>
+<section class="pt-3">
+	<div class="alert alert-primary">
+		Welcome to AgnosUI Demo! Experience the beginning stages of our exciting journey as we actively develop our library for CSS Bootstrap design.
+		<br />
+		Follow us on <a href="https://github.com/AmadeusITGroup/AgnosUI">GitHub</a> for the latest updates and be a part of shaping the next-generation UI
+		components!
+	</div>
+
+	<div class="note my-3">
+		AgnosUI is a powerful library of widgets designed specifically for <a href="https://getbootstrap.com/">Bootstrap</a>. Inspired by the success of
+		<a href="https://ng-bootstrap.github.io/">ng-bootstrap</a>, AgnosUI takes the concept a step further by offering widgets that can seamlessly
+		integrate with any front-end framework of your choice. With support for popular frameworks like <a href="https://angular.io/">Angular</a>,
+		<a href="https://react.dev/">React</a>, and <a href="https://svelte.dev/">Svelte</a>, AgnosUI allows you to effortlessly create consistent and
+		visually appealing UI components across your projects.
+	</div>
+
+	<h4 class="text-center">For a comprehensive understanding of AgnosUI's features and usage,<br />feel free to explore our demo</h4>
+	<div class="d-flex links d-flex justify-content-evenly">
+		<a href="angular/components/alert/examples" aria-label="link to angular examples"><Svg svg={angularLogo} className="framework-logo" /></a>
+		<a href="react/components/alert/examples" aria-label="link to react examples"><Svg svg={reactLogo} className="framework-logo" /></a>
+		<a href="svelte/components/alert/examples" aria-label="link to svelte examples"><Svg svg={sveteLogo} className="framework-logo" /></a>
+	</div>
+</section>
+
+<style lang="scss">
+	.note {
+		font-size: 1.25rem;
+		font-weight: 300;
+	}
+	.links {
+		padding: 1rem;
+		@media (min-width: 768px) {
+			padding: 3rem;
+		}
+
+		:global(.framework-logo) {
+			height: 100px;
+		}
+	}
+</style>
