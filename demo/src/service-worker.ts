d419a0bf64960e5258567ef5b16e27922ad56db2
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2023-08-01 15:33:01 +0200

    demo: in service worker, avoid re-downloading files with the same hash

--- demo/src/service-worker.ts
+++ demo/src/service-worker.ts
@@ -10,11 +10,31 @@ const CACHE = `cache-${version}`;
 
 const ASSETS = [...build.map((file) => file.replace(/\/index\.html$/, '/')), ...prerendered, ...files];
 
+const splitAssets = (assets: string[]) => {
+	const withHash: string[] = [];
+	const withoutHash: string[] = [];
+	const hashRegExp = /[.-]\w{8,}\.\w{2,4}$/;
+	for (const asset of assets) {
+		(hashRegExp.test(asset) ? withHash : withoutHash).push(asset);
+	}
+	return {withHash, withoutHash};
+};
+
 self.addEventListener('install', (event) => {
 	event.waitUntil(
 		(async () => {
 			const cache = await caches.open(CACHE);
-			await cache.addAll(ASSETS);
+			const {withHash, withoutHash} = splitAssets(ASSETS);
+			const missingWithHash: string[] = [];
+			for (const url of withHash) {
+				const response = await caches.match(url);
+				if (response?.ok) {
+					cache.put(url, response);
+				} else {
+					missingWithHash.push(url);
+				}
+			}
+			await cache.addAll([...missingWithHash, ...withoutHash]);
 			await self.skipWaiting();
 		})()
 	);
