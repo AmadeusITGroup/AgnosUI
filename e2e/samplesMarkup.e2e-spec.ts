d6041aa5c0251172d28f0f95d00fc45e2424d17e
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2023-08-02 11:02:31 +0200

    test: correctly check the markup of modal examples

--- e2e/samplesMarkup.e2e-spec.ts
+++ e2e/samplesMarkup.e2e-spec.ts
@@ -1,16 +1,29 @@
-import {test, expect} from '@playwright/test';
-import {htmlSnapshot} from './htmlSnapshot';
+import type {Page} from '@playwright/test';
+import {expect, test} from '@playwright/test';
 import {globSync} from 'glob';
 import path from 'path';
+import {openDemoModal} from './demo-po/modal.po';
+import {htmlSnapshot} from './htmlSnapshot';
 
 test.describe.parallel(`Samples markup consistency check`, () => {
 	const allRoutes = globSync('**/*.route.svelte', {cwd: path.join(__dirname, '../svelte/demo/samples')}).map(
 		(route) => `${route.replace(/\.route\.svelte$/, '').toLowerCase()}`
 	);
+
+	const routesExtraHash: Record<string, string> = {
+		'modal/playground': '#{"props":{"visible":true,"slotDefault":"Dialog content","slotTitle":"Dialog"}}',
+	};
+
+	const routesExtraAction: Record<string, (page: Page) => Promise<void>> = {
+		'modal/default': openDemoModal,
+		'modal/stack': openDemoModal,
+	};
+
 	for (const route of allRoutes) {
 		test(`${route} should have a consistent markup`, async ({page}) => {
 			test.skip(route === 'pagination/pagination', 'FIXME: sample to be made consistent');
-			await page.goto(`#/${route}`);
+			await page.goto(`#/${route}${routesExtraHash[route] ?? ''}`);
+			await routesExtraAction[route]?.(page);
 			await page.waitForSelector('.fade', {state: 'detached'}); // wait for fade transitions to be finished
 			expect(await htmlSnapshot(page.locator('body'))).toMatchSnapshot(`${route}.html`);
 		});
