1a20671f741222da2dccfbbf28276f6b4877fd9d
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2023-08-17 12:10:44 +0200

    demo: adding support for multiple-files samples for stackblitz
    And this also fixes some missing npm dependencies in samples

--- demo/scripts/includeSamples.plugin.ts
+++ demo/scripts/includeSamples.plugin.ts
@@ -1,7 +1,18 @@
 import type {Plugin} from 'vite';
 import path from 'path';
+import type {Frameworks} from '$lib/stores';
+import {readFile} from 'fs/promises';
 
 const samplePrefix = '@agnos-ui/samples/';
+
+const importRegExp = /import([^;]+from)?\s*['"]([^'"]+)['"]\s*;/g;
+const findDependencies = (fileContent: string) => {
+	const dependencies: string[] = [];
+	for (const dependency of fileContent.matchAll(importRegExp)) {
+		dependencies.push(dependency[2]);
+	}
+	return dependencies;
+};
 
 export const includeSamples = (): Plugin => {
 	return {
@@ -19,30 +30,53 @@ export const includeSamples = (): Plugin => {
 				}
 				const [componentName, sampleName] = parts;
 				const normalizedSampleName = `${sampleName[0].toUpperCase()}${sampleName.substring(1)}`;
+
+				const files: Record<Frameworks, {fileName: string; filePath: string}[]> = {} as any;
+				const addFile = async (framework: Frameworks, fileName: string, filePath: string) => {
+					if (!files[framework]) {
+						files[framework] = [];
+					}
+					if (files[framework].some((file) => file.filePath === filePath)) return;
+					files[framework].push({fileName, filePath});
+					this.addWatchFile(filePath);
+					const fileContent = await readFile(filePath, {encoding: 'utf8'});
+					const dependencies = findDependencies(fileContent);
+					const directory = path.dirname(filePath);
+					for (const dependency of dependencies) {
+						const dependencyParts = dependency.split('/');
+						if (dependencyParts[0] === '.') {
+							await addFile(framework, path.basename(dependency), path.join(directory, dependency));
+						} else {
+							// TODO: check that the dependency is valid and included in package.json
+						}
+					}
+				};
+
+				await addFile(
+					'angular',
+					`${sampleName}.component.ts`,
+					path.join(__dirname, `../../angular/demo/src/app/samples/${componentName}/${sampleName}.route.ts`)
+				);
+				await addFile(
+					'react',
+					`${sampleName}.tsx`,
+					path.join(__dirname, `../../react/demo/app/samples/${componentName}/${normalizedSampleName}.route.tsx`)
+				);
+				await addFile(
+					'svelte',
+					`${sampleName}.svelte`,
+					path.join(__dirname, `../../svelte/demo/samples/${componentName}/${normalizedSampleName}.route.svelte`)
+				);
+
 				let output = `export default {componentName:${JSON.stringify(componentName)},sampleName:${JSON.stringify(sampleName)},files:{`;
-
-				// TODO: add support for multiple files in the samples
-
-				let fileName = JSON.stringify(`${sampleName}.component.ts`);
-				output += `angular:{entryPoint: ${fileName}, files: {`;
-				output += `[${fileName}]:() => import(${JSON.stringify(
-					path.join(__dirname, `../../angular/demo/src/app/samples/${componentName}/${sampleName}.route.ts?raw`)
-				)}).then(file=>file.default),`;
-				output += '}},';
-
-				fileName = JSON.stringify(`${sampleName}.tsx`);
-				output += `react:{entryPoint: ${fileName}, files: {`;
-				output += `[${fileName}]:() => import(${JSON.stringify(
-					path.join(__dirname, `../../react/demo/app/samples/${componentName}/${normalizedSampleName}.route.tsx?raw`)
-				)}).then(file=>file.default),`;
-				output += '}},';
-
-				fileName = JSON.stringify(`${sampleName}.svelte`);
-				output += `svelte:{entryPoint: ${fileName}, files: {`;
-				output += `[${fileName}]:() => import(${JSON.stringify(
-					path.join(__dirname, `../../svelte/demo/samples/${componentName}/${normalizedSampleName}.route.svelte?raw`)
-				)}).then(file=>file.default),`;
-				output += '}},';
+				(Object.keys(files) as Frameworks[]).forEach((framework) => {
+					const frameworkFiles = files[framework];
+					output += `${JSON.stringify(framework)}:{entryPoint:${JSON.stringify(frameworkFiles[0].fileName)},files:{`;
+					frameworkFiles.forEach(({fileName, filePath}) => {
+						output += `[${JSON.stringify(fileName)}]: () => import(${JSON.stringify(filePath + '?raw')}).then(file=>file.default),`;
+					});
+					output += `}},`;
+				});
 
 				output += `}};`;
 				return output;
