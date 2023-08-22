8ce6032dbff7ec914aca168e75cd9bae4fabfa99
fbasso <fabrice.basso@amadeus.com>
2023-08-22 11:39:29 +0200

    demo: accordion sample simplification

--- angular/demo/src/app/samples/accordion/default.route.ts
+++ angular/demo/src/app/samples/accordion/default.route.ts
@@ -6,37 +6,19 @@ import {Component} from '@angular/core';
 	imports: [AgnosUIAngularModule],
 	template: `
 		<div auAccordion>
-			<div au-accordion-item [itemCollapsed]="false">
-				<ng-template auAccordionItemHeader>Simple</ng-template>
-				<ng-template auAccordionItemBody
-					>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat
-					skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
-					single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-					proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably
-					haven't heard of them accusamus labore sustainable VHS.
-				</ng-template>
+			<div au-accordion-item [itemCollapsed]="false" [itemId]="'item-1'">
+				<ng-template auAccordionItemHeader>Simple header</ng-template>
+				<ng-template auAccordionItemBody>First content</ng-template>
 			</div>
 			<div au-accordion-item>
 				<ng-template auAccordionItemHeader
 					><span>&#9733; <b>Fancy</b> title &#9733;</span></ng-template
 				>
-				<ng-template auAccordionItemBody
-					>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat
-					skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
-					single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-					proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably
-					haven't heard of them accusamus labore sustainable VHS.
-				</ng-template>
+				<ng-template auAccordionItemBody>Second content</ng-template>
 			</div>
 			<div au-accordion-item [itemDisabled]="true">
 				<ng-template auAccordionItemHeader>Disabled</ng-template>
-				<ng-template auAccordionItemBody
-					>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat
-					skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
-					single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-					proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably
-					haven't heard of them accusamus labore sustainable VHS.
-				</ng-template>
+				<ng-template auAccordionItemBody>Third content</ng-template>
 			</div>
 		</div>
 	`,
--- react/demo/app/samples/accordion/Default.route.tsx
+++ react/demo/app/samples/accordion/Default.route.tsx
@@ -1,14 +1,6 @@
 import {Accordion, AccordionItem} from '@agnos-ui/react';
 
-const body = () => (
-	<>
-		Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
-		dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-		assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
-		butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven&apos;t heard of them
-		accusamus labore sustainable VHS.
-	</>
-);
+const secondSlot = () => <>Second content</>;
 
 const fancyHeader = () => (
 	<span>
@@ -18,9 +10,9 @@ const fancyHeader = () => (
 
 const AccordionDemo = () => (
 	<Accordion>
-		<AccordionItem slotItemHeader="Simple" slotItemBody={body} itemCollapsed={false} />
-		<AccordionItem slotItemHeader={fancyHeader} slotItemBody={body} />
-		<AccordionItem slotItemHeader="Disabled" slotItemBody={body} itemDisabled={true} />
+		<AccordionItem slotItemHeader="Simple header" slotItemBody={'First content'} itemCollapsed={false} itemId={'item-1'} />
+		<AccordionItem slotItemHeader={fancyHeader} slotItemBody={secondSlot} itemId={'item-2'} />
+		<AccordionItem slotItemHeader="Disabled" slotItemBody={'Third content'} itemDisabled={true} itemId={'item-3'} />
 	</Accordion>
 );
 export default AccordionDemo;
