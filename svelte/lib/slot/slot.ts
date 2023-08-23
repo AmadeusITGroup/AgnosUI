4ce568b1c8ed505fe703f825605e71538e4a26de
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2023-08-23 16:46:59 +0200

    fix(svelte): partly fixes SSR with svelte

--- svelte/lib/slot/slot.ts
+++ svelte/lib/slot/slot.ts
@@ -40,5 +40,10 @@ export type AdaptWidgetSlots<W extends Widget> = Widget<
 export const isSvelteComponent = <Props extends object>(content: SlotContent<Props>): content is SlotSvelteComponent<Props> => {
 	// in prod mode, a svelte component has $set on its prototype
 	// in dev mode with hmr (hot module reload), a svelte component has nothing on its prototype, but its name starts with Proxy<
-	return typeof content === 'function' && !!content.prototype && (content.prototype.$set || /^Proxy</.test(content.name));
+	return (
+		(typeof content === 'function' && !!content.prototype && (content.prototype.$set || /^Proxy</.test(content.name))) ||
+		// when using Server Side Rendering, a svelte component is an object with a render function:
+		// (cf https://svelte.dev/docs/server-side-component-api)
+		typeof (content as any)?.render === 'function'
+	);
 };
