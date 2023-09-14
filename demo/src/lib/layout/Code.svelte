c5ed683e7a7540e4e9501dbeb2a63054e5704b7d
Florent EXBRAYAT <florent.exbrayat@amadeus.com>
2023-09-14 11:23:46 +0200

    feat(doc): Add installation guides

--- demo/src/lib/layout/Code.svelte
+++ demo/src/lib/layout/Code.svelte
@@ -1,21 +1,30 @@
 <script lang="ts">
 	import './code.scss';
 	import hljs, {languageFromFileName} from './highlight';
+	import {tooltip} from '$lib/tooltip/tooltip';
+	import clipboard from 'bootstrap-icons/icons/clipboard.svg?raw';
+	import Svg from './Svg.svelte';
 
 	export let code: string;
 	export let fileName: string | undefined = undefined;
 	export let language: string | undefined = undefined;
-	export let className = 'doc p-1';
 
 	let container: HTMLElement;
 	$: appliedLanguage = language ?? languageFromFileName(fileName);
 	$: formattedCode = appliedLanguage ? hljs.highlight(code, {language: appliedLanguage}).value : null;
 </script>
 
-<div class={`bg-light-subtle ${className}`}>
-	<pre class="mb-0"><code bind:this={container}
+<div class={`bg-light-subtle doc p-1 d-flex flex-nowrap align-items-start py-2  px-2 px-sm-4`}>
+	<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+	<pre class="mb-0 align-self-center" tabindex="0"><code bind:this={container}
 			>{#if formattedCode != null}<!-- eslint-disable-line svelte/no-at-html-tags -->{@html formattedCode}{:else}{code}{/if}</code
 		></pre>
+	<button
+		class="btn flex-grow-0 flex-shrink-0 ms-auto"
+		aria-label="copy to clipboard"
+		use:tooltip={{content: 'Copy to clipboard'}}
+		on:click={() => navigator.clipboard.writeText(code)}><Svg className={`align-middle icon-20`} svg={clipboard} /></button
+	>
 </div>
 
 <style lang="scss">
@@ -24,9 +33,6 @@
 	}
 
 	pre {
-		> code {
-			white-space: pre-wrap;
-		}
 		// color: black;
 		tab-size: 1rem;
 	}
