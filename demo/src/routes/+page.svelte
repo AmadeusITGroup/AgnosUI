a1f0fb89a953d5f6c3742008a3bba631c8fda066
ExFlo <florent.exbrayat@amadeus.com>
2023-09-19 14:44:51 +0200

    feat(doc): Add installation guides (#139)

--- demo/src/routes/+page.svelte
+++ demo/src/routes/+page.svelte
@@ -14,82 +14,85 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<div class="container-xxl">
-	<section class="row pres mb-4">
-		<div class="col-12 col-lg-6 order-1 order-lg-0">
-			<div class="pres-logo d-block text-center text-lg-start">AgnosUI</div>
-			<div class="mt-3">
-				<div class="pres-line ps-4 pres-logo-text"><Svg svg={bullet} className="bullet" /> Making your webapps powerfull and consistent</div>
-				<div class="pres-line ps-4 pres-logo-text"><Svg svg={bullet} className="bullet" /> Headless widgets library</div>
-				<div class="pres-line ps-4 pres-logo-text"><Svg svg={bullet} className="bullet" /> Adapters for Angular, React and Svelte</div>
+<!-- TODO make a list of widget and iterate over it when needed -->
+<div class="container-xxl d-flex flex-column align-items-center pt-5">
+	<h1 class="text-center mb-5 mx-sm-4">
+		<span><Svg svg={agnosUILogo} className="agnosui-logo" />gnosUI, where web widgets shine</span>
+	</h1>
+	<h2 class="text-center mx-sm-5 pe-5">Perfected open-source widget libraries with</h2>
+	<ul class="au-landing-list">
+		<li class="d-flex align-items-center">
+			<Svg svg={bullet} className="bullet me-3" />configurable Typescript widget factories
+		</li>
+		<li class="d-flex align-items-center">
+			<Svg svg={bullet} className="bullet me-3" />headless wrappers for Angular, React and Svelte
+		</li>
+		<li class="d-flex align-items-center">
+			<Svg svg={bullet} className="bullet me-3" />Bootstrap-compliant ready-to-use widgets for Angular, React and Svelte.
+		</li>
+	</ul>
+	<div class="card-body au-landing-card p-4 rounded mt-4 mx-3">
+		<h5 class="card-title pres-card-title mb-4">Get started with</h5>
+		<div class="card-text row">
+			<div class="col-12 col-sm-4">
+				<a
+					href="angular/getting-started/installation"
+					class="btn-doc d-flex align-items-center justify-content-center"
+					aria-label="link to angular examples"
+					title="Angular"
+				>
+					<Svg svg={angularLogo} className="pres-card-logo me-3" />
+					Angular
+				</a>
 			</div>
-			<div class="btn-docs card mb-3 mt-5">
-				<div class="row g-0">
-					<div class="col">
-						<div class="card-body">
-							<h5 class="card-title pres-card-title mb-4">Checkout the docs</h5>
-							<div class="card-text row">
-								<div class="col-12 col-sm-4">
-									<a
-										href="angular/components/accordion/examples"
-										class="btn-doc d-flex align-items-center justify-items-center"
-										aria-label="link to angular examples"
-										title="Angular"
-									>
-										<Svg svg={angularLogo} className="pres-card-logo me-3" />
-										Angular
-									</a>
-								</div>
-								<div class="col-12 col-sm-4">
-									<a
-										href="react/components/accordion/examples"
-										class="btn-doc d-flex align-items-center justify-items-center"
-										aria-label="link to angular examples"
-										title="Angular"
-									>
-										<Svg svg={reactLogo} className="pres-card-logo me-3" />
-										React
-									</a>
-								</div>
-								<div class="col-12 col-sm-4">
-									<a
-										href="svelte/components/accordion/examples"
-										class="btn-doc d-flex align-items-center justify-items-center"
-										aria-label="link to angular examples"
-										title="Svelte"
-									>
-										<Svg svg={svelteLogo} className="pres-card-logo me-3" />
-										Svelte
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+			<div class="col-12 col-sm-4">
+				<a
+					href="react/getting-started/installation"
+					class="btn-doc d-flex align-items-center justify-content-center"
+					aria-label="link to angular examples"
+					title="Angular"
+				>
+					<Svg svg={reactLogo} className="pres-card-logo me-3" />
+					React
+				</a>
 			</div>
-		</div>
-		<div class="col-12 col-lg-6 order-0 order-lg-1 d-flex justify-content-center align-items-center">
-			<Svg svg={agnosUILogo} className="agnosui-logo" />
-		</div>
-	</section>
-</div>
-<section class="background pt-5 mt-5 flex-grow-1">
-	<div class="container-xxl">
-		<div class="cards row align-content-around">
-			<LandingCard title="Headless widgets">
-				Each widget is managed in pure typescript with a reactive model and an api to drive the data.
-			</LandingCard>
-			<LandingCard title="Consistent">
-				Widgets are provided for Angular, React and Svelte with the exact same markup and functionnalities.
-			</LandingCard>
-			<LandingCard title="Highly configurable">
-				Default values for all widget props are configurable at any point of the component subtree.
-			</LandingCard>
-			<LandingCard title="Fully tested">Unit test for headless with Vitest. E2e tests for adapters with Playwright.</LandingCard>
-			<LandingCard title="Accessible">Widgets are tested with Axe.</LandingCard>
+			<div class="col-12 col-sm-4">
+				<a
+					href="svelte/getting-started/installation"
+					class="btn-doc d-flex align-items-center justify-content-center"
+					aria-label="link to angular examples"
+					title="Svelte"
+				>
+					<Svg svg={svelteLogo} className="pres-card-logo me-3" />
+					Svelte
+				</a>
+			</div>
 		</div>
 	</div>
-</section>
+</div>
+<footer class="au-footer pt-5 mt-5 flex-grow-1">
+	<div class="container-xxl">
+		<div class="cards row align-content-around">
+			<LandingCard title="Framework agnostic">
+				A framework-agnostic core of widget factories that provide reactive models and APIs to drive the data. You can use it with any front-end
+				framework.
+			</LandingCard>
+			<LandingCard title="Headless widgets">
+				Each widget is provided in headless mode in Angular, React and Svelte using the same factory. You can use them with any front-end CSS
+				framework.
+			</LandingCard>
+			<LandingCard title="Bootstrap widgets">
+				Highly customizable Bootstrap CSS widgets are available for Angular, React and Svelte in separated packages.
+			</LandingCard>
+			<LandingCard title="Consistent">Widgets are provided for Angular, React and Svelte with the exact same markup and functionalities.</LandingCard>
+			<LandingCard title="Highly configurable">
+				Default values for all widgets props are configurable at any point of the application subtree.
+			</LandingCard>
+			<LandingCard title="Fully tested">Unit tests for headless with Vitest. E2e tests for adapters with Playwright.</LandingCard>
+			<LandingCard title="Accessible">Widgets are tested for accessibility with Axe.</LandingCard>
+		</div>
+	</div>
+</footer>
 
 <style lang="scss">
 	@import '../../../common/variables';
@@ -103,48 +106,25 @@
 	}
 
 	:global(.agnosui-logo) {
-		min-width: 200px;
-		max-width: 320px;
-		width: 30vw;
+		width: 50px;
 	}
-
-	.pres {
-		padding-top: 1rem;
-
-		&-card-title {
-			font-size: 1.5rem;
-			text-align: center;
-		}
-
-		&-logo {
-			font-family: $font-name, sans-serif;
-			font-size: 5rem;
-			font-weight: 300;
-			background: linear-gradient($gradient-text-top, $gradient-text-bottom);
-			// background: linear-gradient(#bc0cf1, #d40404);
-			background-clip: text;
-			-webkit-background-clip: text;
-			-webkit-text-fill-color: transparent;
-		}
-
-		&-line {
-			font-size: 1.3rem;
-			margin-bottom: 0.5rem;
-			@include media-breakpoint-up(md) {
-				font-size: 1.6rem !important;
-			}
-
-			:global(.bullet) {
-				height: 20px;
-				color: $gradient-text-top;
-			}
-		}
+	.pres-card-title {
+		font-size: 1.5rem;
+		text-align: center;
 	}
-
-	.btn-docs {
+	.au-landing-card {
 		background-color: $light-background;
+		border: 1px solid var(--bs-primary);
 	}
-
+	.au-landing-list {
+		list-style-type: none;
+		font-size: 1rem;
+		margin-bottom: 0.5rem;
+		:global(.bullet) {
+			height: 20px;
+			color: $gradient-text-top;
+		}
+	}
 	.btn-doc {
 		font-family: 'Jost', sans-serif;
 		font-weight: 500;
@@ -157,23 +137,14 @@
 		margin: 5px 0 0 0;
 		color: #fff;
 		background: $gradient-top;
-		// border: 4px solid $gradient-bottom;
-		// border: 5px solid #1066ef;
 		text-decoration: none;
 	}
-
 	.btn-doc:hover {
 		background: $gradient-bottom;
 		border-color: $gradient-top;
 	}
-
-	.background {
+	.au-footer {
 		border-top: 1px solid $gradient-text-bottom;
 		background-color: $light-background;
 	}
-
-	// 	:global(.framework-logo) {
-	// 		height: 50px;
-	// 	}
-	// }
 </style>
---
+++ demo/src/routes/[framework]/getting-started/installation/fwk/svelte/NewProject.svelte
@@ -0,0 +1,1 @@
+<a href="https://svelte.dev/docs/introduction" target="_blank">Svelte</a>
