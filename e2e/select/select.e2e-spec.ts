2ee232cbe70cabbbdd72c0d0a86fe5ad30e39da5
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2023-09-29 13:22:32 +0200

    fix(e2e): use expect.poll to wait for the state to match expectations

--- e2e/select/select.e2e-spec.ts
+++ e2e/select/select.e2e-spec.ts
@@ -21,7 +21,7 @@ test.describe.parallel(`Select tests`, () => {
 			list: [],
 		};
 
-		expect(await selectPO.state()).toEqual(expectedState);
+		await expect.poll(() => selectPO.state()).toEqual(expectedState);
 
 		const locatorInput = selectPO.locatorInput;
 		await locatorInput.fill('a');
@@ -35,13 +35,13 @@ test.describe.parallel(`Select tests`, () => {
 				{text: 'Action 3', hasCheckBox: true, isChecked: false},
 			],
 		});
-		expect(await selectPO.state()).toEqual(expectedState);
+		await expect.poll(() => selectPO.state()).toEqual(expectedState);
 
 		await locatorInput.press('Enter');
 
 		expectedState.badges = [['Action 1', 'x']];
 		expectedState.list[0].isChecked = true;
-		expect(await selectPO.state()).toEqual(expectedState);
+		await expect.poll(() => selectPO.state()).toEqual(expectedState);
 
 		await page.locator('body').click({position: {x: 0, y: 0}});
 
@@ -49,23 +49,23 @@ test.describe.parallel(`Select tests`, () => {
 			isOpen: false,
 			list: [],
 		});
-		expect(await selectPO.state()).toEqual(expectedState);
+		await expect.poll(() => selectPO.state()).toEqual(expectedState);
 	});
 
 	test(`Close actions`, async ({page}) => {
 		const selectPO = new SelectPO(page);
 		await selectPO.locatorInput.fill('a');
-		expect((await selectPO.state()).isOpen).toEqual(true);
+		await expect.poll(async () => (await selectPO.state()).isOpen).toEqual(true);
 
 		await selectPO.locatorMenuItem('action 1').click();
 		await selectPO.locatorMenuItem('action 2').click();
-		expect((await selectPO.state()).isOpen).toEqual(true);
+		await expect.poll(async () => (await selectPO.state()).isOpen).toEqual(true);
 
 		await selectPO.locatorBadgeItemCross('action 1').click();
-		expect((await selectPO.state()).isOpen).toEqual(true);
+		await expect.poll(async () => (await selectPO.state()).isOpen).toEqual(true);
 
 		await page.locator('body').click({position: {x: 0, y: 0}});
-		expect((await selectPO.state()).isOpen).toEqual(false);
+		await expect.poll(async () => (await selectPO.state()).isOpen).toEqual(false);
 	});
 
 	test(`Config`, async ({page}) => {
@@ -79,27 +79,27 @@ test.describe.parallel(`Select tests`, () => {
 			list: [],
 		};
 
-		expect(await selectPO.state()).toEqual(expectedState);
+		await expect.poll(() => selectPO.state()).toEqual(expectedState);
 		await expect(selectConfigPO.locatorFilterTextInput).toHaveValue(expectedState.text);
 
 		expectedState.text = 'other value';
 		await selectConfigPO.locatorFilterTextInput.fill(expectedState.text);
-		expect(await selectPO.state()).toEqual(expectedState);
+		await expect.poll(() => selectPO.state()).toEqual(expectedState);
 
 		expectedState.text = 'test';
 		await selectPO.locatorInput.fill(expectedState.text);
-		expect(await selectPO.state()).toEqual(expectedState);
+		await expect.poll(() => selectPO.state()).toEqual(expectedState);
 
 		await selectConfigPO.locatorFilterTextInput.fill('next value'); // does not change the select (which now has its own 'test' value)
 		await expect(selectConfigPO.locatorFilterTextInput).toHaveValue('next value');
-		expect(await selectPO.state()).toEqual(expectedState);
+		await expect.poll(() => selectPO.state()).toEqual(expectedState);
 
 		expectedState.text = 'next value';
 		await selectConfigPO.locatorResetFilterTextButton.click();
-		expect(await selectPO.state()).toEqual(expectedState);
+		await expect.poll(() => selectPO.state()).toEqual(expectedState);
 
 		expectedState.text = 'sync again';
 		await selectPO.locatorInput.fill(expectedState.text);
-		expect(await selectPO.state()).toEqual(expectedState);
+		await expect.poll(() => selectPO.state()).toEqual(expectedState);
 	});
 });
