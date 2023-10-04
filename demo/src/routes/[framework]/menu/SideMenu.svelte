a58f4860eb0a6ca764d0349db19e283d55600d78
Basso <fabrice.basso@amadeus.com>
2023-10-04 11:28:45 +0200

    demo: menu improvements (#171)

--- demo/src/routes/[framework]/menu/SideMenu.svelte
+++ demo/src/routes/[framework]/menu/SideMenu.svelte
@@ -9,7 +9,7 @@
 <nav class="w-100 mt-1">
 	{#each menu as { title, submenu }}
 		<strong class="d-flex w-100 align-items-center fw-semibold">{title}</strong>
-		<div>
+		<div class="my-2">
 			{#each submenu as { label, path }}
 				{@const isCurrent = $page.route.id?.startsWith(`/[framework]${path}`)}
 				<a
