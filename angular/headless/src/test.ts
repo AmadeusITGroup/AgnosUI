918d175d49ad3542820c8fedfdd017cbffce5d63
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2023-10-23 11:27:39 +0200

    [wip] vitest patch for zone

--- angular/headless/src/test.ts
+++ angular/headless/src/test.ts
@@ -1,9 +1,13 @@
 // This file is required by vitest
 
 import 'zone.js';
+import './vitest-patch';
 import 'zone.js/testing';
-import {getTestBed} from '@angular/core/testing';
+import {beforeEach} from './vitest-wrapper';
+import {TestBed} from '@angular/core/testing';
 import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
 
-// First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+beforeEach(() => {
+	TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+	return () => TestBed.resetTestEnvironment();
+});
