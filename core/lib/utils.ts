eaca54fd01638ba8fa2b7ef56117bd842b390f3b
Marko Oleksiyenko <markoleksiyenko@gmail.com>
2023-11-16 14:41:12 +0100

    feat(slider): Add decimal values support for slider

--- core/lib/utils.ts
+++ core/lib/utils.ts
@@ -1,2 +1,19 @@
 export const noop = () => {};
 export const identity = <T>(x: T) => x;
+
+const decimalRegExp = /(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/;
+
+/**
+ * @param number - decimal number
+ * @returns the decimal precision of the number
+ */
+export function getDecimalPrecision(number: number) {
+	const matches = ('' + number).match(decimalRegExp);
+	return Math.max(
+		0,
+		// Number of digits right of decimal point.
+		(matches![1]?.length ?? 0) -
+			// Adjust for exponential notation.
+			(+matches![2] || 0),
+	);
+}
