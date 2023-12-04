8274c707b306d3fbc2eeca9e5df17a1469d89623
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2023-12-04 10:50:52 +0100

    fix(demo): keep component active in the menu even when not on "Examples" tab

--- demo/src/routes/[framework]/menu/SideMenu.svelte
+++ demo/src/routes/[framework]/menu/SideMenu.svelte
@@ -10,13 +10,13 @@
 	{#each menu as { title, submenu }}
 		<strong class="d-flex w-100 align-items-center fw-semibold">{title}</strong>
 		<div class="my-2">
-			{#each submenu as { label, path }}
+			{#each submenu as { label, path, subpath }}
 				{@const isCurrent = $page.route.id?.startsWith(`/[framework]${path}`)}
 				<a
 					class="menu-item menu-item-sidenav"
 					class:active={isCurrent}
 					aria-current={isCurrent ? 'page' : undefined}
-					href="{$pathToRoot$}{$selectedFramework$}{path}"
+					href="{$pathToRoot$}{$selectedFramework$}{path}{subpath}"
 				>
 					{label}
 				</a>
