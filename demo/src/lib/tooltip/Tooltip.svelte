4714a5b10c70198a7575e72fbb6f00b34c3367d7
Basso <fabrice.basso@amadeus.com>
2023-12-19 16:52:34 +0100

    demo: replace popperjs by floatingui (#333)

--- demo/src/lib/tooltip/Tooltip.svelte
+++ demo/src/lib/tooltip/Tooltip.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
+	import type {Directive} from '@agnos-ui/core/types';
+	import type {Readable} from '@amadeus-it-group/tansu';
+	import type {Placement} from '@floating-ui/dom';
+
 	export let content: string;
-	export let placement: 'top' | 'right' | 'bottom' | 'left' = 'top';
+	export let placement$: Readable<Placement>;
+	export let directive: Directive;
+	export let arrowDirective: Directive;
 </script>
 
-<div id="tooltip" role="tooltip" class="tooltip bs-tooltip-auto fade show" data-show="" data-popper-placement={placement}>
+<div use:directive role="tooltip" class="tooltip bs-tooltip-auto fade show position-absolute" data-show="" data-popper-placement={$placement$}>
 	<div class="tooltip-inner">
 		{content}
 	</div>
-	<div id="arrow" class="tooltip-arrow" data-popper-arrow />
+	<div use:arrowDirective class="tooltip-arrow position-absolute" data-popper-arrow />
 </div>
