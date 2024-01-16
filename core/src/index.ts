5ad8da4fa031d3a5d0f84ff0dc5510f61c7ccf26
Quentin Deroubaix <quentin.deroubaix@amadeus.com>
2024-01-16 18:09:46 +0100

    feat: default tsconfig with alias, allowing to go to declaration in VSCode

--- core/src/index.ts
+++ core/src/index.ts
@@ -3,14 +3,14 @@ export * from './components/commonProps';
 export * from './types';
 
 // components
-export * from './components/accordion/accordion';
-export * from './components/alert/alert';
-export * from './components/modal/modal';
-export * from './components/pagination/pagination';
-export * from './components/progressbar/progressbar';
-export * from './components/rating/rating';
-export * from './components/select/select';
-export * from './components/slider/slider';
+export * from './components/accordion';
+export * from './components/alert';
+export * from './components/modal';
+export * from './components/pagination';
+export * from './components/progressbar';
+export * from './components/rating';
+export * from './components/select';
+export * from './components/slider';
 
 // config
 export * from './config';
