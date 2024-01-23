8d1a68d9a30787186aa1db14d78d7e6ba11cdf60
Marko Oleksiyenko <markoleksiyenko@gmail.com>
2024-01-23 10:48:02 +0100

    feat(menu): Add collapsible sections to side menu

--- demo/src/routes/menu/SideMenu.svelte
+++ demo/src/routes/menu/SideMenu.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
 	import {page} from '$app/stores';
 	import {pathToRoot$, frameworkLessUrl$, selectedFramework$} from '$lib/stores';
+	import CollapsibleSection from './CollapsibleSection.svelte';
 
 	import './menu.scss';
 
 	$: isAngular = $selectedFramework$ === 'angular';
 	$: isReact = $selectedFramework$ === 'react';
 	$: isSvelte = $selectedFramework$ === 'svelte';
+
+	function toKebabCase(original: string): string {
+		return original
+			.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)!
+			.join('-')
+			.toLowerCase();
+	}
 </script>
 
 <nav class="w-100 mt-1">
@@ -36,19 +44,20 @@
 		</div>
 	{/if}
 	{#each $page.data.menu ?? [] as { title, submenu }}
-		<strong class="d-flex w-100 align-items-center fw-semibold">{title}</strong>
-		<div class="my-2">
-			{#each submenu as { label, path, subpath }}
-				{@const isCurrent = $page.url.pathname?.includes(path)}
-				<a
-					class="menu-item menu-item-sidenav"
-					class:active={isCurrent}
-					aria-current={isCurrent ? 'page' : undefined}
-					href="{$pathToRoot$}{path}{subpath}"
-				>
-					{label}
-				</a>
-			{/each}
-		</div>
+		<CollapsibleSection headerText={title} defaultVisible={$page.url.pathname?.includes(toKebabCase(title))}>
+			<div>
+				{#each submenu as { label, path, subpath }}
+					{@const isCurrent = $page.url.pathname?.includes(path)}
+					<a
+						class="menu-item menu-item-sidenav"
+						class:active={isCurrent}
+						aria-current={isCurrent ? 'page' : undefined}
+						href="{$pathToRoot$}{path}{subpath}"
+					>
+						{label}
+					</a>
+				{/each}
+			</div>
+		</CollapsibleSection>
 	{/each}
 </nav>
