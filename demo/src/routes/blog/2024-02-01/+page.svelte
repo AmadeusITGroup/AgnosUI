8f70950115d494e8fc39981634df63d46157eb1f
Marko Oleksiyenko <markoleksiyenko@gmail.com>
2024-02-26 14:00:05 +0100

    feat(blog): Add Intl support for publish date

--- demo/src/routes/blog/2024-02-01/+page.svelte
+++ demo/src/routes/blog/2024-02-01/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import Header from '$lib/layout/Header.svelte';
+	import Published from '$lib/layout/Published.svelte';
 	import Markdown from '$lib/markdown/Markdown.svelte';
 	import source from './tansu-reactivity.md?raw';
 </script>
 
+<Header title="State management with Tansu" />
+<Published date="02/01/2024" />
 <Markdown {source} />
