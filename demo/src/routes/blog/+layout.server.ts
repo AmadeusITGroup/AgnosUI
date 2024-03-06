8801393e1c84f1ffaf48201950f2d4e36ea62a71
Marko Oleksiyenko <markoleksiyenko@gmail.com>
2024-03-06 16:45:16 +0100

    feat(demo): Add portal version selection dropdown

--- demo/src/routes/blog/+layout.server.ts
+++ demo/src/routes/blog/+layout.server.ts
@@ -6,5 +6,6 @@ export const load = async () => {
 				submenu: [{label: '02-01-2024 State management with Tansu', path: 'blog/2024-02-01', subpath: ''}],
 			},
 		],
+		since: 'v0.1.1',
 	};
 };
---
+++ demo/src/routes/menu/Versions.svelte
@@ -0,0 +1,89 @@
+<script context="module" lang="ts">
+	import {page} from '$app/stores';
+	import {pathToRoot$, selectedFramework$} from '$lib/stores';
+	import {createHasFocus} from '@agnos-ui/core/services/focustrack';
+	import type {Directive} from '@agnos-ui/core/types';
+	import type {ReadableSignal} from '@amadeus-it-group/tansu';
+	import {writable} from '@amadeus-it-group/tansu';
+	import {lte, valid} from 'semver';
+	import {onMount} from 'svelte';
+
+	export interface Version {
+		folder: string;
+		version: string;
+	}
+</script>
+
+<script lang="ts">
+	export let versions: Version[];
+
+	onMount(() => {
+		({hasFocus$, directive} = createHasFocus());
+	});
+
+	const currentVersion$ = writable(versions.find((version) => version.version === `v${import.meta.env.AGNOSUI_VERSION}`) ?? versions[0]);
+	const open$ = writable(false);
+
+	let hasFocus$: ReadableSignal<boolean>;
+	let directive: Directive;
+	$: $open$ = $hasFocus$;
+
+	function giveFocus(el: HTMLAnchorElement, index: number) {
+		if (index === 0) {
+			el.focus();
+		}
+	}
+
+	$: versionsWithUrl = versions.map((version) => ({
+		...version,
+		url:
+			version.version === 'PREVIEW' || ($page.data['since'] && valid($page.data['since']) && lte($page.data['since'], version.version))
+				? $page.url.pathname.replace(`/${$currentVersion$.folder}/`, `/${version.folder}/`)
+				: `${$pathToRoot$.replace($currentVersion$.folder, version.folder)}docs/${$selectedFramework$}/getting-started/introduction`,
+	}));
+</script>
+
+<div class="nav-item ms-3">
+	<div class="dropdown">
+		<button
+			class="btn nav-link dropdown-toggle align-items-center d-flex {$currentVersion$.version === 'PREVIEW' ? 'badge text-bg-warning' : ''}"
+			aria-label="demo version select"
+			on:click={() => ($open$ = !$open$)}
+			type="button"
+			data-bs-toggle="dropdown"
+			aria-expanded={$open$}
+		>
+			{$currentVersion$.version}
+		</button>
+		{#if $open$}
+			<div
+				use:directive
+				class="dropdown-menu dropdown-menu-end bs-popover-auto position-absolute"
+				class:show={$open$}
+				data-popper-placement="bottom-end"
+				data-bs-popper="absolute"
+			>
+				{#each versionsWithUrl as version, index}
+					<a
+						use:giveFocus={index}
+						class="dropdown-item d-flex align-items-center"
+						href={version.url}
+						class:active={version.folder === $currentVersion$.folder}
+						on:click={() => {
+							currentVersion$.set(version);
+							$open$ = !$open$;
+						}}
+					>
+						{index === 1 ? `Latest (${version.version})` : version.version}
+					</a>
+				{/each}
+			</div>
+		{/if}
+	</div>
+</div>
+
+<style lang="css">
+	.nav-link {
+		transition: none;
+	}
+</style>
