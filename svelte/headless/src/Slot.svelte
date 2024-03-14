08116e97cb0413708d84fbfd9b088c9c63d24625
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2024-03-14 15:25:39 +0100

    Revert "fix: workaround for Svelte SSR generation"
    This reverts commit 043869b70164101c1de148c2b75e4ad38a9ff238.

--- svelte/headless/src/Slot.svelte
+++ svelte/headless/src/Slot.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import {BROWSER} from 'esm-env';
 	import type {SlotContent, SlotSvelteComponent} from './types';
-	import {useSvelteSlot} from './types';
 	import {isSvelteComponent} from './utils/widget';
+	import {useSvelteSlot} from './types';
 	type Props = $$Generic<object>; // eslint-disable-line no-undef
 	// cf https://github.com/ota-meshi/eslint-plugin-svelte/issues/348
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -12,13 +11,6 @@
 	};
 	export let slotContent: SlotContent<Props> = null;
 	export let props: Props;
-
-	// Workaround for a svelte issue that prevents code generation for SSR/slot
-	// To be removed when https://github.com/sveltejs/svelte/issues/9137 is fixed
-	if (!BROWSER) {
-		// @ts-expect-error Setting the global variable `props`, which is not defined in the SSR Svelte-kit compilation
-		globalThis['props'] = props;
-	}
 </script>
 
 {#if slotContent === useSvelteSlot}
