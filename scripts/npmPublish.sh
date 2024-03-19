a90e444116cdff38dc03d293e7e128a7c8db9315
divdavem <david-emmanuel.divernois@amadeus.com>
2024-03-19 11:28:49 +0100

    refactor(build): convert npmPublish.sh to npmPublish.js (#605)

--- scripts/npmPublish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-for dir in core/dist style-bootstrap {svelte,react,angular}/{headless,lib}/dist base-po page-objects ; do
-	echo '$ cd '"$dir"' && npm publish --access=public '"$@"
-	( cd "$dir" && npm publish --access=public "$@" )
-done
