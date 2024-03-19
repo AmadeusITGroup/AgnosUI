8b83680a2b2aeb1782099d52f5dccb6a80c5e967
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2024-03-19 11:34:25 +0100

    fix(build): package.json issues

--- scripts/buildPackageJson.js
+++ scripts/buildPackageJson.js
@@ -54,7 +54,7 @@ cleanObjDistPrefix(pkg, 'types');
 if (pkg.exports) {
 	for (const key of Object.keys(pkg.exports)) {
 		const pkgExport = pkg.exports[key];
-		cleanObjDistPrefix(pkgExport, 'types', 'default', 'svelte', 'esm', 'esm2022');
+		cleanObjDistPrefix(pkgExport, 'types', 'default', 'svelte', 'esm', 'esm2022', 'require');
 	}
 }
 
