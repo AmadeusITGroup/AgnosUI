0b2925280381ccb269d5a5d382ba07a838f28d0f
divdavem <david-emmanuel.divernois@amadeus.com>
2024-03-19 17:24:17 +0100

    fix(core): fixing core mergeInto function (#613)

--- core/src/config.spec.ts
+++ core/src/config.spec.ts
@@ -1,6 +1,6 @@
 import {describe, expect, test} from 'vitest';
 import type {Partial2Levels} from './config';
-import {createWidgetsConfig} from './config';
+import {createWidgetsConfig, mergeInto} from './config';
 
 describe('defaultConfig', () => {
 	test(`Basic functionalities`, () => {
@@ -59,5 +59,34 @@ describe('defaultConfig', () => {
 		childCfg$.set({w: {}});
 		expect(parentCfgValues).toEqual([{}, {w: {a: 2}}]);
 		expect(childCfgValues).toEqual([{w: {a: 0}}, {w: {a: 3}}, {w: {a: 9}}, {w: {a: 3}}]);
+	});
+});
+
+describe('mergeInto', () => {
+	test('Basic functionalities', () => {
+		expect(mergeInto(undefined, undefined, 2)).toEqual(undefined);
+		expect(mergeInto(undefined, {}, 2)).toEqual({});
+		expect(mergeInto({}, undefined, 2)).toEqual({});
+		expect(mergeInto(undefined, {rating: {}}, 2)).toEqual({rating: {}});
+		expect(mergeInto({rating: {}}, undefined, 2)).toEqual({rating: {}});
+		expect(mergeInto(undefined, {rating: {className: 'a'}}, 2)).toEqual({rating: {className: 'a'}});
+		expect(mergeInto({rating: {className: 'a'}}, undefined, 2)).toEqual({rating: {className: 'a'}});
+		expect(mergeInto({}, {rating: {className: 'a'}}, 2)).toEqual({rating: {className: 'a'}});
+		expect(mergeInto({rating: {className: 'a'}}, {}, 2)).toEqual({rating: {className: 'a'}});
+		expect(mergeInto({rating: {}}, {rating: {className: 'a'}}, 2)).toEqual({rating: {className: 'a'}});
+		expect(mergeInto({rating: {className: 'a'}}, {rating: {}}, 2)).toEqual({rating: {className: 'a'}});
+		expect(mergeInto({rating: {className: 'b'}}, {rating: {className: 'a'}}, 2)).toEqual({rating: {className: 'a'}});
+		expect(mergeInto({rating: {className: 'b'}}, {rating: {className: undefined}}, 2)).toEqual({rating: {className: 'b'}});
+		expect(mergeInto({rating: {}}, {rating: {className: undefined}}, 2)).toEqual({rating: {className: undefined}});
+		expect(mergeInto({rating: {className: undefined}}, {rating: {}}, 2)).toEqual({rating: {className: undefined}});
+		expect(mergeInto({rating: {className: undefined}}, {rating: {min: 5}}, 2)).toEqual({rating: {className: undefined, min: 5}});
+		expect(mergeInto({rating: {extra: {a: 1}}}, {rating: {extra: {b: 2}}}, 2)).toEqual({rating: {extra: {b: 2}}});
+		expect(mergeInto({a: {}}, {a: 'b'}, 2)).toEqual({a: 'b'});
+	});
+
+	test('Invalid cases', () => {
+		expect(() => {
+			mergeInto<string | {a: string}>('a', {a: 'b'}, 2);
+		}).toThrow();
 	});
 });
