b0839aea7c49ecf835cf0eef9822d12d2e067029
Quentin Deroubaix <quentin.deroubaix@amadeus.com>
2024-03-25 17:42:55 +0100

    fix: iframe default heights

--- demo/src/routes/docs/[framework]/daisyUI/rating/headless/+page.svelte
+++ demo/src/routes/docs/[framework]/daisyUI/rating/headless/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import tailwind from '@agnos-ui/samples/daisyui/rating/default';
+	import sampleDaisyUI from '@agnos-ui/samples/daisyui/rating/default';
 	import Sample from '$lib/layout/Sample.svelte';
 	import Section from '$lib/layout/Section.svelte';
 </script>
 
 <Section label="DaisyUI" id="default" level={2}>
-	<Sample title="DaisyUI example" sample={tailwind} height={395} />
+	<Sample title="DaisyUI example" sample={sampleDaisyUI} height={127} />
 </Section>
