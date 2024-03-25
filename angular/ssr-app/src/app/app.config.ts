67d14ea02adff8af4cdf6b9b38d794b0ed80ed0c
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2024-03-25 17:23:10 +0100

    build: angular SSR app

---
+++ angular/ssr-app/src/app/app.config.ts
@@ -0,0 +1,7 @@
+import type {ApplicationConfig} from '@angular/core';
+
+import {provideClientHydration} from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+	providers: [provideClientHydration()],
+};
