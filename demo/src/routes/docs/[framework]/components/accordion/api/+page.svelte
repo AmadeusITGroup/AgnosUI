db697d9d55776fc517d277c2b03d097477774c31
Quentin Deroubaix <quentin.deroubaix@amadeus.com>
2024-04-04 11:46:36 +0200

    fix(alert, toast, doc): default values in api doc for alert and toast

--- demo/src/routes/docs/[framework]/components/accordion/api/+page.svelte
+++ demo/src/routes/docs/[framework]/components/accordion/api/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-	import {getAccordionDefaultConfig} from '@agnos-ui/svelte/components/accordion';
 	import widgetDoc from '@agnos-ui/generated/doc/api.json?accordion&extractApi';
 	import ApiDoc from '$lib/layout/doc/ApiDoc.svelte';
 </script>
 
-<ApiDoc doc={widgetDoc} defaultValues={getAccordionDefaultConfig()} />
+<ApiDoc doc={widgetDoc} />
--- demo/src/routes/docs/[framework]/components/modal/api/+page.svelte
+++ demo/src/routes/docs/[framework]/components/modal/api/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-	import {getModalDefaultConfig} from '@agnos-ui/svelte/components/modal';
 	import widgetDoc from '@agnos-ui/generated/doc/api.json?modal&extractApi';
 	import ApiDoc from '$lib/layout/doc/ApiDoc.svelte';
 </script>
 
-<ApiDoc doc={widgetDoc} defaultValues={getModalDefaultConfig()} />
+<ApiDoc doc={widgetDoc} />
