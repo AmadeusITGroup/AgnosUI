5f5701d65e6c163fdecef06ff003a223e2d97300
Andrea SCORTI <andrea.scorti@amadeus.com>
2024-04-19 16:22:43 +0200

    feat(alert): daisyUI transition + directives

--- svelte/demo/src/daisyui/samples/alert/Alert.svelte
+++ svelte/demo/src/daisyui/samples/alert/Alert.svelte
@@ -2,15 +2,25 @@
 	import {createAlert} from '@agnos-ui/svelte-headless/components/alert';
 	import type {AlertProps} from '@agnos-ui/svelte-headless/components/alert';
 	import {callWidgetFactory} from '@agnos-ui/svelte-headless/config';
+	import {createSimpleClassTransition} from '@agnos-ui/svelte-headless/services/transitions/simpleClassTransition';
 	type $$Props = Partial<Pick<AlertProps, 'className' | 'visible' | 'dismissible' | 'ariaCloseButtonLabel'>>;
+
+	const transition = createSimpleClassTransition({
+		showClasses: ['transition-opacity'],
+		hideClasses: ['opacity-0'],
+		animationPendingHideClasses: ['opacity-0', 'transition-opacity'],
+	});
+
 	export const {
 		stores: {className$, dismissible$, ariaCloseButtonLabel$, hidden$},
+		directives: {transitionDirective},
 		patchChangedProps,
 		api,
 	} = callWidgetFactory({
 		factory: createAlert,
 		widgetName: 'alert',
 		$$props,
+		defaultConfig: {transition},
 		events: {
 			onVisibleChange: (event) => {
 				visible = event;
@@ -24,7 +34,7 @@
 </script>
 
 {#if !$hidden$}
-	<div role="alert" class="alert {$className$}">
+	<div role="alert" class="alert {$className$}" use:transitionDirective>
 		<slot />
 		{#if $dismissible$}
 			<button class="btn btn-sm btn-circle btn-ghost justify-self-end" on:click={api.close} aria-label={$ariaCloseButtonLabel$}>
