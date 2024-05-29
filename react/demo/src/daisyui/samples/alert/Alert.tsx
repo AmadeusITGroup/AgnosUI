4ee1122930352a413780a7914fa1ff79fe3426fa
divdavem <david-emmanuel.divernois@amadeus.com>
2024-05-29 23:41:09 +0200

    refactor(react): inline useDirective (#785)

--- react/demo/src/daisyui/samples/alert/Alert.tsx
+++ react/demo/src/daisyui/samples/alert/Alert.tsx
@@ -1,10 +1,22 @@
 import type {AlertProps} from '@agnos-ui/react-headless/components/alert';
 import {createAlert} from '@agnos-ui/react-headless/components/alert';
+import type {AlertWidget, AlertState} from '@agnos-ui/react-headless/components/alert';
 import {useWidgetWithConfig} from '@agnos-ui/react-headless/config';
 import {createSimpleClassTransition} from '@agnos-ui/react-headless/services/transitions/simpleClassTransition';
 import {useDirective} from '@agnos-ui/react-headless/utils/directive';
 import CloseIcon from '@agnos-ui/common/samples/common/close_icon.svg?react';
 import type {PropsWithChildren} from 'react';
+
+const AlertContent = ({widget, state, children}: PropsWithChildren<{widget: AlertWidget; state: AlertState}>) => (
+	<div role="alert" className={`flex alert ${state.className}`} {...useDirective(widget.directives.transitionDirective)}>
+		{children}
+		{state.dismissible && (
+			<button className="btn btn-sm btn-circle btn-ghost ms-auto" onClick={widget.api.close} aria-label={`${state.ariaCloseButtonLabel}`}>
+				<CloseIcon />
+			</button>
+		)}
+	</div>
+);
 
 export const Alert = (
 	props: PropsWithChildren<Partial<Pick<AlertProps, 'className' | 'visible' | 'dismissible' | 'ariaCloseButtonLabel' | 'onVisibleChange'>>>,
@@ -16,18 +28,12 @@ export const Alert = (
 	});
 
 	const [state, widget] = useWidgetWithConfig(createAlert, props, 'alert', {transition});
-	const transitionDirective = useDirective(widget.directives.transitionDirective);
 	return (
 		<>
 			{!state.hidden && (
-				<div role="alert" className={`flex alert ${state.className}`} {...transitionDirective}>
+				<AlertContent widget={widget} state={state}>
 					{props.children}
-					{state.dismissible && (
-						<button className="btn btn-sm btn-circle btn-ghost ms-auto" onClick={widget.api.close} aria-label={`${state.ariaCloseButtonLabel}`}>
-							<CloseIcon />
-						</button>
-					)}
-				</div>
+				</AlertContent>
 			)}
 		</>
 	);
