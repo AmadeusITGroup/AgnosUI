cc7deb10b0715d671a43307333badb1b56d65d2d
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2024-07-12 14:39:00 +0200

    feat(eslint-plugin): eslint rule to check default values in tsdoc

--- eslint-plugin/src/index.ts
+++ eslint-plugin/src/index.ts
@@ -1,4 +1,5 @@
 import {angularCheckPropsRule} from './angular-check-props';
+import {checkDefaultPropsRule} from './check-default-props';
 import {checkReplaceImportsRule} from './check-replace-imports';
 import {svelteCheckPropsRule} from './svelte-check-props';
 import {svelteCheckSlotsRule} from './svelte-check-slots';
@@ -6,6 +7,7 @@ import {svelteCheckSlotsRule} from './svelte-check-slots';
 export default {
 	rules: {
 		'angular-check-props': angularCheckPropsRule,
+		'check-default-props': checkDefaultPropsRule,
 		'check-replace-imports': checkReplaceImportsRule,
 		'svelte-check-props': svelteCheckPropsRule,
 		'svelte-check-slots': svelteCheckSlotsRule,
---
+++ eslint-plugin/test/check-default-props.spec.ts
@@ -0,0 +1,270 @@
+import type {InvalidTestCase} from '@typescript-eslint/rule-tester';
+import {RuleTester} from '@typescript-eslint/rule-tester';
+import type {TSESLint} from '@typescript-eslint/utils';
+import {afterAll, describe, test} from 'vitest';
+import {checkDefaultPropsRule} from '../src/check-default-props';
+
+RuleTester.describe = describe;
+RuleTester.it = test;
+RuleTester.afterAll = afterAll;
+
+describe('check-default-propss', () => {
+	const ruleTester = new RuleTester({
+		parser: require.resolve('@typescript-eslint/parser'),
+		parserOptions: {
+			project: './tsconfig.test.json',
+			tsconfigRootDir: __dirname,
+		},
+	});
+
+	type MessageIds<T extends TSESLint.RuleModule<any, any>> = T extends TSESLint.RuleModule<infer U, any> ? U : never;
+
+	const invalid: InvalidTestCase<MessageIds<typeof checkDefaultPropsRule>, []>[] = [
+		{
+			code: `
+export interface MyType {
+	a: number;
+}
+export function getMyWidgetDefaultConfig(): MyType {
+	return {a: 1};
+}`,
+			output: `
+export interface MyType {
+	/**
+	 *
+	 * @defaultValue \`1\`
+	 */
+	a: number;
+}
+export function getMyWidgetDefaultConfig(): MyType {
+	return {a: 1};
+}`,
+			errors: [
+				{
+					messageId: 'missingDefaultValue',
+					data: {
+						propName: 'a',
+						defaultValue: '1',
+					},
+				},
+			],
+		},
+		{
+			code: `
+export interface MyType {
+	/**
+	 * Very useful property
+	 */
+	a: number;
+}
+export function getMyWidgetDefaultConfig(): MyType {
+	return {a: 2};
+}`,
+			output: `
+export interface MyType {
+	/**
+	 * Very useful property
+	 *
+	 * @defaultValue \`2\`
+	 */
+	a: number;
+}
+export function getMyWidgetDefaultConfig(): MyType {
+	return {a: 2};
+}`,
+			errors: [
+				{
+					messageId: 'missingDefaultValue',
+					data: {
+						propName: 'a',
+						defaultValue: '2',
+					},
+				},
+			],
+		},
+		{
+			code: `
+export interface MyType {
+	/**
+	 * Very useful property
+	 *
+	 * @defaultValue \`2\`
+	 */
+	a: number;
+}
+const defValues = {a: 3};
+export function getMyWidgetDefaultConfig(): MyType {
+	return defValues;
+}`,
+			output: `
+export interface MyType {
+	/**
+	 * Very useful property
+	 *
+	 * @defaultValue \`3\`
+	 */
+	a: number;
+}
+const defValues = {a: 3};
+export function getMyWidgetDefaultConfig(): MyType {
+	return defValues;
+}`,
+			errors: [
+				{
+					messageId: 'incorrectDefaultValue',
+					data: {
+						propName: 'a',
+						foundValue: '2',
+						defaultValue: '3',
+					},
+				},
+			],
+		},
+		{
+			code: `
+export interface MyType {
+	/**
+	 * Very useful property
+	 *
+	 * @defaultValue \`2\`
+	 */
+	a: number | undefined;
+}
+const defValues = {a: undefined};
+export function getMyWidgetDefaultConfig(): MyType {
+	return defValues;
+}`,
+			output: `
+export interface MyType {
+	/**
+	 * Very useful property
+	 */
+	a: number | undefined;
+}
+const defValues = {a: undefined};
+export function getMyWidgetDefaultConfig(): MyType {
+	return defValues;
+}`,
+			errors: [
+				{
+					messageId: 'expectedNoDefaultValue',
+					data: {
+						propName: 'a',
+					},
+				},
+			],
+		},
+		{
+			code: `
+export interface MyType1 {
+	/**
+	 * My super property
+	 */
+	propA: any;
+}
+export interface MyType2 extends MyType1 {}
+function myOtherFn() {
+	return {
+		propA: 'myDefaultValue'
+	} as any;
+}
+const myFn = () => ({...myOtherFn()});
+
+export function getMyWidgetDefaultConfig(): MyType2 {
+	return myFn();
+}
+`,
+			output: `
+export interface MyType1 {
+	/**
+	 * My super property
+	 *
+	 * @defaultValue \`'myDefaultValue'\`
+	 */
+	propA: any;
+}
+export interface MyType2 extends MyType1 {}
+function myOtherFn() {
+	return {
+		propA: 'myDefaultValue'
+	} as any;
+}
+const myFn = () => ({...myOtherFn()});
+
+export function getMyWidgetDefaultConfig(): MyType2 {
+	return myFn();
+}
+`,
+			errors: [
+				{
+					messageId: 'missingDefaultValue',
+					data: {
+						propName: 'propA',
+						defaultValue: "'myDefaultValue'",
+					},
+				},
+			],
+		},
+		{
+			code: `
+export interface MyType {
+	/**
+	 * My mysterious property
+	 */
+	mysteriousProp: any;
+}
+export function getMyWidgetDefaultConfig(): MyType {
+	return window.myConfig;
+}
+`,
+			errors: [{messageId: 'staticAnalysisError', data: {error: 'unexpected kind of expression'}}],
+		},
+		{
+			code: `
+export interface MyType {
+	/**
+	 * My mysterious property
+	 */
+	mysteriousProp: any;
+}
+export function getMyWidgetDefaultConfig(): MyType {
+	return {[window.myProp]: 'myValue'};
+}
+`,
+
+			errors: [{messageId: 'staticAnalysisError', data: {error: 'unexpected kind of property'}}],
+		},
+		{
+			code: `
+export interface MyType {
+	/**
+	 * My mysterious property
+	 */
+	mysteriousProp: any;
+}
+export function getMyWidgetDefaultConfig(): MyType {
+	if (window.something) {
+		return {mysteriousProp: 'myValue1'};
+	}
+	return {mysteriousProp: 'myValue2'};
+}
+`,
+
+			errors: [{messageId: 'staticAnalysisError', data: {error: 'unexpected kind of function'}}],
+		},
+	];
+
+	ruleTester.run('check-default-props', checkDefaultPropsRule, {
+		valid: [
+			{
+				code: '',
+			},
+			...invalid
+				.filter(({output}) => !!output)
+				.map(({output}) => ({
+					code: output!,
+				})),
+		],
+		invalid,
+	});
+});
