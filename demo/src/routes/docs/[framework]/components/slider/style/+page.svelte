9b818e18e8e77b033efce00d388bb150dfeb30da
Quentin Deroubaix <139884126+quentinderoubaix@users.noreply.github.com>
2024-07-29 14:23:17 +0200

    feat: standalone styles (#873)

--- demo/src/routes/docs/[framework]/components/slider/style/+page.svelte
+++ demo/src/routes/docs/[framework]/components/slider/style/+page.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
 	import Section from '$lib/layout/Section.svelte';
 	import Style from '$lib/layout/Style.svelte';
-	import sliderCssVars from '@agnos-ui/styles/core-bootstrap/_slider?slider-css-vars';
+	import sliderCssVars from '@agnos-ui/styles/core-bootstrap/slider?slider-css-vars';
 	import sliderVars from '@agnos-ui/styles/core-bootstrap/_variables?slider-vars';
 </script>
+
+<p>
+	You will need to import the styles defined in <code>@agnos-ui/core-bootstrap/css/slider.css</code> or
+	<code>@agnos-ui/core-bootstrap/scss/slider.scss</code>
+	<br />
+	These styles are also exported in the global export <code>@agnos-ui/core-bootstrap/css/agnosui.css</code> or
+	<code>@agnos-ui/core-bootstrap/scss/agnosui.scss</code>
+</p>
 
 <Section label="CSS Variables" level={2}>
 	<Style style={sliderCssVars}></Style>
