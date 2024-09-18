40aa54d0ac8e4cbf03cb5c04b59e7978e762bc7b
qderoubaix <quentin.deroubaix@amadeus.com>
2024-09-18 16:36:02 +0200

    feat(svelte/headless): svelte 5 migration

--- svelte/headless/src/Slot.svelte
+++ svelte/headless/src/Slot.svelte
@@ -1,24 +1,19 @@
-<script lang="ts">
-	import type {SlotContent, SlotSvelteComponent} from './types';
-	import {isSvelteComponent} from './utils/widget';
-	import {useSvelteSlot} from './types';
-	type Props = $$Generic<object>;
-	// cf https://github.com/ota-meshi/eslint-plugin-svelte/issues/348
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	type $$Slots = {
-		default: {component: SlotSvelteComponent<Props>; props: Props};
-		slot: {props: Props};
-	};
-	export let slotContent: SlotContent<Props> = null;
-	export let props: Props;
+<script lang="ts" generics="Props extends object">
+	import type {SlotContent} from './types';
+
+	let {
+		content,
+		props,
+	}: {
+		content: SlotContent<Props>;
+		props: Props;
+	} = $props();
 </script>
 
-{#if slotContent === useSvelteSlot}
-	<slot name="slot" {props} />
-{:else if typeof slotContent === 'string'}
-	{slotContent}
-{:else if slotContent && !isSvelteComponent(slotContent)}
-	{slotContent(props)}
-{:else if slotContent}
-	<slot component={slotContent} {props} />
+{#if content}
+	{#if typeof content === 'string'}
+		{content}
+	{:else}
+		{@render content(props)}
+	{/if}
 {/if}
