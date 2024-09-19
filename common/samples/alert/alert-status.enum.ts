5824b19cdbf9febe9996cbb8d3936b54eb4b1b0f
Quentin Deroubaix <quentin.deroubaix@amadeus.com>
2024-09-19 10:08:56 +0200

    feat: move duplicated alert and toast enums to the common folder

---
+++ common/samples/alert/alert-status.enum.ts
@@ -0,0 +1,10 @@
+export enum AlertStatus {
+	success = 'success',
+	info = 'info',
+	warning = 'warning',
+	danger = 'danger',
+	primary = 'primary',
+	secondary = 'secondary',
+	light = 'light',
+	dark = 'dark',
+}
