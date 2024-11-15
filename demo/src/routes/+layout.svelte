4bbfc610b958818c8de20be29dc4371d8b5b8a15
Quentin Deroubaix <quentin.deroubaix@amadeus.com>
2024-11-15 14:28:58 +0100

    feat: analytics and bluesky link

--- demo/src/routes/+layout.svelte
+++ demo/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Svg from '$lib/layout/Svg.svelte';
 	import github from 'bootstrap-icons/icons/github.svg?raw';
+	import bluesky from '$resources/bluesky.svg?raw';
 	import twitter from 'bootstrap-icons/icons/twitter-x.svg?raw';
 	import {canonicalURL$, pathToRoot$, routeLevel$, selectedFramework$, selectedApiFramework$} from '$lib/stores';
 	import './styles.scss';
@@ -133,6 +134,9 @@
 				</a>
 				<a class="nav-link" href="https://twitter.com/AgnosUI" aria-label="link to twitter / x account" target="_blank">
 					<Svg className="icon-24 align-middle" svg={twitter} />
+				</a>
+				<a class="nav-link" href="https://bsky.app/profile/agnosui.bsky.social" aria-label="link to bluesky profile" target="_blank">
+					<Svg className="icon-24 align-middle" svg={bluesky} />
 				</a>
 			</div>
 			<div class="align-items-center d-flex d-md-none gap-3">
