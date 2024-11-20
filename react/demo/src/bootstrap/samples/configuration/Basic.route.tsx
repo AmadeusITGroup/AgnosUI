17bf6f49a986fc60c03070e535a9d51852136e3f
Quentin Deroubaix <quentin.deroubaix@amadeus.com>
2024-11-20 09:18:24 +0100

    feat: showcase react bug

--- react/demo/src/bootstrap/samples/configuration/Basic.route.tsx
+++ react/demo/src/bootstrap/samples/configuration/Basic.route.tsx
@@ -1,10 +1,20 @@
 import {Alert} from '@agnos-ui/react-bootstrap/components/alert';
 import {WidgetsDefaultConfig} from '@agnos-ui/react-bootstrap/config';
+import {useState} from 'react';
 
-const BasicDemo = () => (
-	<WidgetsDefaultConfig alert={{dismissible: false}}>
-		<Alert type="primary">Simple primary alert</Alert>
-		<Alert type="secondary">Simple secondary alert</Alert>
-	</WidgetsDefaultConfig>
-);
+const BasicDemo = () => {
+	const [dismissible, setDismissible] = useState(false);
+	return (
+		<>
+			<button className="btn btn-primary" onClick={() => setDismissible((a) => !a)}>
+				Toggle config dismissible
+			</button>
+			<div className="my-2">Config dissmissible is {dismissible ? 'true' : 'false'}</div>
+			<WidgetsDefaultConfig alert={{dismissible}}>
+				<Alert type="primary">Simple primary alert</Alert>
+				<Alert type="secondary">Simple secondary alert</Alert>
+			</WidgetsDefaultConfig>
+		</>
+	);
+};
 export default BasicDemo;
