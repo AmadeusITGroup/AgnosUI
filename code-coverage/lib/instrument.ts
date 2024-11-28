9392d196d2e8c0d44481683682b4248ff8efe50c
Quentin Deroubaix <quentin.deroubaix@amadeus.com>
2024-11-28 21:12:52 +0100

    fix: coverage tests when using new signal apis

--- code-coverage/lib/instrument.ts
+++ code-coverage/lib/instrument.ts
@@ -1,14 +1,49 @@
-import {transform} from '@babel/core';
+import {parse, transformFromAstSync, type PluginItem} from '@babel/core';
 import {dirname} from 'path';
+import {type CallExpression} from '@babel/types';
+
+const excludedAngularCalls = ['input', 'output', 'viewChild', 'viewChildren', 'contentChild', 'contentChildren'];
 
 // TODO: support for svelte files
 const supportedExtensionsRegExp = /\.(tsx?|jsx?)$/;
 export const canInstrument = (fileName: string) => !fileName.includes('\x00') && supportedExtensionsRegExp.test(fileName);
 
+function ignoreCallExpression(callExpression: CallExpression): boolean {
+	if (callExpression.callee.type === 'MemberExpression') {
+		return (
+			callExpression.callee.object.type === 'Identifier' &&
+			excludedAngularCalls.includes(callExpression.callee.object.name) &&
+			callExpression.callee.property.type === 'Identifier' &&
+			callExpression.callee.property.name === 'required'
+		);
+	}
+	if (callExpression.callee.type === 'Identifier') {
+		return excludedAngularCalls.includes(callExpression.callee.name);
+	}
+	return false;
+}
+
+function ignoreSomeAngular(): PluginItem {
+	return {
+		visitor: {
+			ClassProperty(path) {
+				if (path.node.value?.type === 'CallExpression' && ignoreCallExpression(path.node.value)) {
+					if (!path.node.leadingComments) {
+						path.node.leadingComments = [];
+					}
+					path.node.leadingComments.push({
+						type: 'CommentBlock',
+						value: ' istanbul ignore next ',
+					});
+				}
+			},
+		},
+	};
+}
+
 export const instrumentFile = (code: string, filename: string) => {
 	console.log('Instrumenting for coverage: ', filename);
-	const result = transform(code, {
-		filename,
+	const parsedFile = parse(code, {
 		plugins: [
 			['@babel/plugin-syntax-decorators', {version: 'legacy'}],
 			[
@@ -17,6 +52,18 @@ export const instrumentFile = (code: string, filename: string) => {
 					isTSX: filename.endsWith('.tsx'),
 				},
 			],
+		],
+	});
+	if (!parsedFile) {
+		return code;
+	}
+	const transformedAst = transformFromAstSync(parsedFile, code, {plugins: [ignoreSomeAngular()], ast: true})?.ast;
+	if (!transformedAst) {
+		return code;
+	}
+	const result = transformFromAstSync(transformedAst, code, {
+		filename,
+		plugins: [
 			[
 				'babel-plugin-istanbul',
 				{
