b5b93b21a9cc20294eecac6b5c35422d8dfc32b1
Andrea SCORTI <andrea.scorti@amadeus.com>
2024-12-06 15:14:10 +0100

    feat: add docsearch

---
+++ demo/src/lib/docsearch/Search.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import {onMount} from 'svelte';
+	import docsearch from '@docsearch/js';
+	import {selectedFramework$} from '$lib/stores';
+
+	function generateUniqueId() {
+		return 'id-' + Math.random().toString(36).slice(2, 11);
+	}
+	let uniqueId = generateUniqueId();
+
+	onMount(() => {
+		selectedFramework$.subscribe((value) => {
+			docsearch({
+				container: `#docsearch-${uniqueId}`,
+				appId: 'B76VWEHQMR',
+				indexName: 'agnosui',
+				apiKey: 'e82efd11bdb692114cb75b90299504b8',
+				searchParameters: {
+					indexName: 'agnosui',
+					facetFilters: [`${value ? 'tags:' + value : ''}`],
+				},
+			});
+		});
+	});
+</script>
+
+<div id="docsearch-{uniqueId}"></div>
