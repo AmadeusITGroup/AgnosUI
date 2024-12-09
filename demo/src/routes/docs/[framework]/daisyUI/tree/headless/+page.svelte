43291e120cae24ce68ebddb12a8886a72eb2c30f
Marko Oleksiyenko <markoleksiyenko@gmail.com>
2024-12-09 16:54:07 +0100

    feat(tree): Add headless DaisyUI examples

---
+++ demo/src/routes/docs/[framework]/daisyUI/tree/headless/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import daisyuiSample from '@agnos-ui/samples/daisyui/tree/default';
+	import Sample from '$lib/layout/Sample.svelte';
+	import Section from '$lib/layout/Section.svelte';
+</script>
+
+<Section label="DaisyUI" id="default" level={2}>
+	<Sample title="DaisyUI example" sample={daisyuiSample} height={256} />
+</Section>
