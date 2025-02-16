1a2cd503b1c18413c3b6bbb6b6ce44b02dbe9f45
qderoubaix <quentin.deroubaix@amadeus.com>
2025-02-16 22:17:36 +0100

    feat(demo): shiki jsengine + github default themes

--- demo/src/lib/layout/highlight.ts
+++ demo/src/lib/layout/highlight.ts
@@ -1,12 +1,23 @@
-import {createHighlighter, type ThemeInput} from 'shiki';
-import lightPlus from './light-plus.json' with {type: 'json'};
-import darkPlus from './dark-plus.json' with {type: 'json'};
+import {createHighlighterCore} from 'shiki/core';
+import {createJavaScriptRegexEngine} from 'shiki/engine/javascript';
+import shikiAngularTS from '@shikijs/langs/angular-ts';
+import shikiAngularHTML from '@shikijs/langs/angular-html';
+import shikiTsx from '@shikijs/langs/tsx';
+import shikiHtml from '@shikijs/langs/html';
+import shikiCss from '@shikijs/langs/css';
+import shikiScss from '@shikijs/langs/scss';
+import shikiSvelte from '@shikijs/langs/svelte';
+import shikiBash from '@shikijs/langs/bash';
+import shikiTypescript from '@shikijs/langs/typescript';
+import shikiThemeLight from '@shikijs/themes/github-light-default';
+import shikiThemeDark from '@shikijs/themes/github-dark-default';
 
 export const langs = ['angular-ts', 'angular-html', 'tsx', 'html', 'css', 'scss', 'svelte', 'bash', 'typescript', 'ts'];
 
-const highlighter = await createHighlighter({
-	langs,
-	themes: [lightPlus as ThemeInput, darkPlus as ThemeInput],
+const highlighter = await createHighlighterCore({
+	langs: [shikiAngularTS, shikiAngularHTML, shikiTsx, shikiHtml, shikiCss, shikiScss, shikiSvelte, shikiBash, shikiTypescript],
+	themes: [shikiThemeLight, shikiThemeDark],
+	engine: createJavaScriptRegexEngine(),
 });
 export default highlighter;
 
