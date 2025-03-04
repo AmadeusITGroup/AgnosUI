fc619ee73b79bd46f2cc2866fd723d2c75c21cdd
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2025-03-04 16:26:39 +0100

    fix(demo): use $props.id() rather than hard-coding an id in svelte collapse

--- svelte/demo/src/bootstrap/samples/collapse/Default.route.svelte
+++ svelte/demo/src/bootstrap/samples/collapse/Default.route.svelte
@@ -2,9 +2,10 @@
 	import {Collapse} from '@agnos-ui/svelte-bootstrap/components/collapse';
 
 	let visible = $state(true);
+	const id = $props.id();
 </script>
 
-<button class="btn btn-primary mb-2" type="button" aria-controls="auId-0" aria-expanded={visible} onclick={() => (visible = !visible)}
+<button class="btn btn-primary mb-2" type="button" aria-controls={id} aria-expanded={visible} onclick={() => (visible = !visible)}
 	>Toggle collapse</button
 >
-<Collapse {visible} onHidden={() => console.log('Hidden')} id="auId-0"><div class="card card-body">Visible content</div></Collapse>
+<Collapse {visible} onHidden={() => console.log('Hidden')} {id}><div class="card card-body">Visible content</div></Collapse>
