1584b689aa57f6d7deff78711226d70e9ab9f104
Andrea SCORTI <andrea.scorti@amadeus.com>
2025-03-18 15:44:52 +0100

    fix: update layout for the example

--- angular/demo/bootstrap/src/app/samples/toast/toaster.route.ts
+++ angular/demo/bootstrap/src/app/samples/toast/toaster.route.ts
@@ -7,31 +7,44 @@ import {FormsModule} from '@angular/forms';
 	template: `
 		<p class="mb-2">Similar to dynamic stacking, we expose a service to ease the usage of Toasts</p>
 		<div class="d-flex flex-column justify-content-between gap-2">
-			<div class="d-flex form-group align-items-center gap-3">
-				<label class="me-3" for="positionSelect">Position: </label>
-				<select id="positionSelect" class="form-select w-auto" [(ngModel)]="position">
-					@for (position of positions; track position) {
-						<option [value]="position.value">{{ position.label }}</option>
-					}
-				</select>
-				<label class="me-3" for="dismissible">Dismissible: </label>
-				<input type="checkbox" class="form-check-input" id="dismissible" [disabled]="duration() === 0" [(ngModel)]="dismissible" />
-				<label class="me-3" for="duration">Duration: </label>
-				<input id="duration" type="number" class="form-control w-auto" [(ngModel)]="duration" />
+			<div class="d-flex flex-column flex-sm-row form-group align-items-center gap-3">
+				<div class="d-flex w-100 align-items-center justify-content-between">
+					<label class="me-3" for="positionSelect">Position: </label>
+					<select id="positionSelect" class="form-select w-auto" [(ngModel)]="position">
+						@for (position of positions; track position) {
+							<option [value]="position.value">{{ position.label }}</option>
+						}
+					</select>
+				</div>
+				<div class="d-flex w-100 align-items-center justify-content-between">
+					<label class="me-3" for="dismissible">Dismissible: </label>
+					<input type="checkbox" class="form-check-input" id="dismissible" [disabled]="duration() === 0" [(ngModel)]="dismissible" />
+				</div>
+				<div class="d-flex w-100 align-items-center justify-content-between">
+					<label class="me-3" for="duration">Duration: </label>
+					<input id="duration" type="number" class="form-control w-50" [(ngModel)]="duration" />
+				</div>
 			</div>
-			<div class="d-flex form-group align-items-center gap-3">
-				<label class="me-3" for="limit">Max # of toasts: </label>
-				<input id="limit" type="number" class="form-control w-auto" [(ngModel)]="limit" />
-				<label class="me-3" for="pause">Pause timer on hover: </label>
-				<input type="checkbox" class="form-check-input" id="pause" [(ngModel)]="pauseOnHover" />
-				<label class="me-3" for="closeAll">Close all toasts button</label>
-				<input type="checkbox" class="form-check-input" id="closeAll" [(ngModel)]="closeAll" />
+			<div class="d-flex flex-column flex-sm-row form-group align-items-center gap-3">
+				<div class="d-flex w-100 align-items-center justify-content-between">
+					<label class="me-3" for="limit">Max # of toasts: </label>
+					<input id="limit" type="number" class="form-control w-50" [(ngModel)]="limit" />
+				</div>
+				<div class="d-flex w-100 align-items-center justify-content-between">
+					<label class="me-3" for="pause">Pause timer on hover: </label>
+					<input type="checkbox" class="form-check-input" id="pause" [(ngModel)]="pauseOnHover" />
+				</div>
+				<div class="d-flex w-100 align-items-center justify-content-between">
+					<label class="me-3" for="closeAll">Close all toasts button</label>
+					<input type="checkbox" class="form-check-input" id="closeAll" [(ngModel)]="closeAll" />
+				</div>
 			</div>
 			<div class="d-flex form-group align-items-center gap-3 py-3">
 				<button class="btn btn-primary ms-2" (click)="addToast()">Show toast</button>
 				<button class="btn btn-primary ms-2" (click)="addToast('error')">Show error toast</button>
 			</div>
 		</div>
+		<div class="d-flex position-relative mt-2 w-100" style="height: 500px; background-color: gray;"></div>
 		<au-component
 			#toaster
 			auToaster
--- react/demo/src/bootstrap/samples/toast/Toaster.route.tsx
+++ react/demo/src/bootstrap/samples/toast/Toaster.route.tsx
@@ -47,81 +47,94 @@ const ToasterDemo = () => {
 		<ToasterProvider options={options}>
 			<p className="mb-2">Similar to dynamic stacking, we expose a service to ease the usage of Toasts</p>
 			<div className="d-flex flex-column justify-content-between gap-2">
-				<div className="d-flex form-group align-items-center gap-3">
-					<label className="me-3" htmlFor="positionSelect">
-						Position:{' '}
-					</label>
-					<select
-						id="positionSelect"
-						className="form-select w-auto"
-						value={options.position}
-						onChange={(e) => setOptions({...options, position: e.target.value as ToastPositions})}
-					>
-						{positions.map(({value, label}) => (
-							<option key={value} value={value}>
-								{' '}
-								{label}
-							</option>
-						))}
-					</select>
-					<label className="me-3" htmlFor="dismissible">
-						Dismissible:{' '}
-					</label>
-					<input
-						type="checkbox"
-						className="form-check-input"
-						id="dismissible"
-						disabled={options.duration === 0}
-						checked={options.dismissible}
-						onChange={(e) => setOptions({...options, dismissible: e.target.checked})}
-					/>
-					<label className="me-3" htmlFor="duration">
-						Duration:{' '}
-					</label>
-					<input
-						id="duration"
-						type="number"
-						className="form-control w-auto"
-						value={options.duration}
-						onChange={(e) => setOptions({...options, duration: Number(e.target.value)})}
-					/>
+				<div className="d-flex flex-column flex-sm-row form-group align-items-center gap-3">
+					<div className="d-flex w-100 align-items-center justify-content-between">
+						<label className="me-3" htmlFor="positionSelect">
+							Position:{' '}
+						</label>
+						<select
+							id="positionSelect"
+							className="form-select w-auto"
+							value={options.position}
+							onChange={(e) => setOptions({...options, position: e.target.value as ToastPositions})}
+						>
+							{positions.map(({value, label}) => (
+								<option key={value} value={value}>
+									{' '}
+									{label}
+								</option>
+							))}
+						</select>
+					</div>
+					<div className="d-flex w-100 align-items-center justify-content-between">
+						<label className="me-3" htmlFor="dismissible">
+							Dismissible:{' '}
+						</label>
+						<input
+							type="checkbox"
+							className="form-check-input"
+							id="dismissible"
+							disabled={options.duration === 0}
+							checked={options.dismissible}
+							onChange={(e) => setOptions({...options, dismissible: e.target.checked})}
+						/>
+					</div>
+					<div className="d-flex w-100 align-items-center justify-content-between">
+						<label className="me-3" htmlFor="duration">
+							Duration:{' '}
+						</label>
+						<input
+							id="duration"
+							type="number"
+							className="form-control w-50"
+							value={options.duration}
+							onChange={(e) => setOptions({...options, duration: Number(e.target.value)})}
+						/>
+					</div>
 				</div>
-				<div className="d-flex form-group align-items-center gap-3">
-					<label className="me-3" htmlFor="limit">
-						Max # of toasts:{' '}
-					</label>
-					<input
-						id="limit"
-						type="number"
-						className="form-control w-auto"
-						value={options.limit}
-						onChange={(e) => setOptions({...options, limit: Number(e.target.value)})}
-					/>
-					<label className="me-3" htmlFor="pause">
-						Pause timer on hover:{' '}
-					</label>
-					<input
-						type="checkbox"
-						className="form-check-input"
-						id="pause"
-						checked={options.pauseOnHover}
-						onChange={(e) => setOptions({...options, pauseOnHover: e.target.checked})}
-					/>
-					<label className="me-3" htmlFor="closeAll">
-						Close all toasts button
-					</label>
-					<input
-						type="checkbox"
-						className="form-check-input"
-						id="closeAll"
-						checked={options.closeAll}
-						onChange={(e) => setOptions({...options, closeAll: e.target.checked})}
-					/>
+				<div className="d-flex flex-column flex-sm-row form-group align-items-center gap-3">
+					<div className="d-flex w-100 align-items-center justify-content-between">
+						<label className="me-3" htmlFor="limit">
+							Max # of toasts:{' '}
+						</label>
+						<input
+							id="limit"
+							type="number"
+							className="form-control w-50"
+							value={options.limit}
+							onChange={(e) => setOptions({...options, limit: Number(e.target.value)})}
+						/>
+					</div>
+					<div className="d-flex w-100 align-items-center justify-content-between">
+						<label className="me-3" htmlFor="pause">
+							Pause timer on hover:{' '}
+						</label>
+						<input
+							type="checkbox"
+							className="form-check-input"
+							id="pause"
+							checked={options.pauseOnHover}
+							onChange={(e) => setOptions({...options, pauseOnHover: e.target.checked})}
+						/>
+					</div>
+					<div className="d-flex w-100 align-items-center justify-content-between">
+						<label className="me-3" htmlFor="closeAll">
+							Close all toasts button
+						</label>
+						<input
+							type="checkbox"
+							className="form-check-input"
+							id="closeAll"
+							checked={options.closeAll}
+							onChange={(e) => setOptions({...options, closeAll: e.target.checked})}
+						/>
+					</div>
 				</div>
 				<div className="d-flex form-group align-items-center gap-3 py-3">
 					<ToasterActions />
 				</div>
 			</div>
+			<div className="d-flex position-relative mt-2 w-100" style={{height: '500px', backgroundColor: 'gray'}}></div>
 		</ToasterProvider>
 	);
 };
