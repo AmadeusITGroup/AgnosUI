15341f6e0544af7cebffe2da3800d1263257cf50
Quentin Deroubaix <quentin.deroubaix@amadeus.com>
2025-03-28 15:14:23 +0100

    fix(demo): TOC + flash in dropdown

--- demo/src/routes/menu/TOC.svelte
+++ demo/src/routes/menu/TOC.svelte
@@ -8,46 +8,80 @@
 	const elements = fromStore(elements$);
 	const visibleElements = fromStore(visibleElements$);
 
-	const tocElements = $derived(
-		elements.current.map((element) => {
+	type TOC = {
+		href: string;
+		label: string;
+		isVisible: boolean;
+		submenu: {
+			href: string;
+			label: string;
+			isVisible: boolean;
+		}[];
+	};
+
+	const tocElements = $derived.by(() => {
+		const tocs: TOC[] = [];
+		elements.current.forEach((element) => {
 			const isVisible = visibleElements.current.has(element);
-			const header = element.querySelector('h1,h2,h3') as HTMLElement;
+			const header = element.querySelector('h2,h3') as HTMLHeadingElement;
 			if (header) {
-				const classnames = [`menu-item menu-item-toc hover:bg-secondary-subtle! block rounded-end toc-${header.tagName.toLowerCase()}`];
-
-				if (isVisible) {
-					classnames.push('active');
+				if (header.tagName.toLowerCase() === 'h2') {
+					tocs.push({
+						label: header.innerText,
+						href: header.querySelector('a')?.getAttribute('href') ?? '',
+						isVisible,
+						submenu: [],
+					});
+				} else {
+					const last = tocs[tocs.length - 1];
+					last.submenu.push({
+						label: header.innerText,
+						href: header.querySelector('a')?.getAttribute('href') ?? '',
+						isVisible,
+					});
 				}
-
-				return {
-					label: header.innerText,
-					href: header.querySelector('a')?.getAttribute('href') ?? '',
-					classname: classnames.join(' '),
-					isVisible,
-				};
-			} else {
-				return {
-					label: '',
-					href: '',
-					classname: '',
-					isVisible,
-				};
 			}
-		}),
-	);
+		});
+		return tocs;
+	});
 </script>
 
 {#if elements.current.length}
 	<div class="ms-2 mt-1 border-start-1">
-		<div class="hidden lg:block font-bold pb-2 mb-1">On this page</div>
-		<div>
-			{#each tocElements as { label, classname, href, isVisible }}
-				{#if label && href}
-					<a class={classname} aria-current={isVisible ? 'page' : undefined} {href}>
+		<div class="hidden lg:block font-bold mb-1">On this page</div>
+		<ul class="menu ps-0">
+			{#each tocElements as { label, href, isVisible, submenu } (href)}
+				<li class="mb-0!">
+					<a
+						class={[
+							'menu-item menu-item-toc hover:bg-secondary-subtle! focus-visible:bg-secondary-subtle! block rounded-none rounded-end',
+							isVisible && 'active',
+						]}
+						aria-current={isVisible ? 'page' : undefined}
+						{href}
+					>
 						{label}
 					</a>
-				{/if}
+					{#if submenu.length}
+						<ul class="ps-0">
+							{#each submenu as { label, href, isVisible } (href)}
+								<li class="mb-0!">
+									<a
+										class={[
+											'menu-item menu-item-toc hover:bg-secondary-subtle! focus-visible:bg-secondary-subtle! block rounded-none rounded-end',
+											isVisible && 'active',
+										]}
+										aria-current={isVisible ? 'page' : undefined}
+										{href}
+									>
+										{label}
+									</a>
+								</li>
+							{/each}
+						</ul>
+					{/if}
+				</li>
 			{/each}
-		</div>
+		</ul>
 	</div>
 {/if}
