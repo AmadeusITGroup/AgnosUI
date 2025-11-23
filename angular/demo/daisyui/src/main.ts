7214b694f2b3ff7ce30dd79790a9df398d957a85
Quentin Deroubaix <quentin.deroubaix@amadeus.com>
2025-11-23 09:50:02 +0100

    refactor: removing zone.js from most places

--- angular/demo/daisyui/src/main.ts
+++ angular/demo/daisyui/src/main.ts
@@ -3,13 +3,8 @@ import {bootstrapApplication} from '@angular/platform-browser';
 import {provideRouter} from '@angular/router';
 import {AppComponent} from './app/app.component';
 import {ROUTES} from './app/app.routes';
-import {provideBrowserGlobalErrorListeners, provideZonelessChangeDetection} from '@angular/core';
+import {provideBrowserGlobalErrorListeners} from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-	providers: [
-		provideRouter(ROUTES),
-		{provide: LocationStrategy, useClass: HashLocationStrategy},
-		provideBrowserGlobalErrorListeners(),
-		provideZonelessChangeDetection(),
-	],
+	providers: [provideRouter(ROUTES), {provide: LocationStrategy, useClass: HashLocationStrategy}, provideBrowserGlobalErrorListeners()],
 }).catch((err) => console.error(err));
--- angular/ssr-app/src/app/app.config.ts
+++ angular/ssr-app/src/app/app.config.ts
@@ -1,7 +1,7 @@
-import {provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, type ApplicationConfig} from '@angular/core';
+import {provideBrowserGlobalErrorListeners, type ApplicationConfig} from '@angular/core';
 
 import {provideClientHydration} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideClientHydration(), provideBrowserGlobalErrorListeners(), provideZonelessChangeDetection()],
+	providers: [provideClientHydration(), provideBrowserGlobalErrorListeners()],
 };
