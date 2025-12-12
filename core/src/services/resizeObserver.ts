296ff4fd0c91785c44dd311b7ff1e7326d32dd71
Marko Oleksiyenko <markoleksiyenko@gmail.com>
2025-12-12 15:16:14 +0100

    feat: Add multiple resize observer directive

--- core/src/services/resizeObserver.ts
+++ core/src/services/resizeObserver.ts
@@ -1,6 +1,6 @@
 import type {ReadableSignal} from '@amadeus-it-group/tansu';
 import {derived} from '@amadeus-it-group/tansu';
-import {createBrowserStoreDirective} from '../utils/directive';
+import {createBrowserStoreArrayDirective, createBrowserStoreDirective} from '../utils/directive';
 import {noop} from '../utils/func';
 import type {Directive, SSRHTMLElement} from '../types';
 
@@ -40,6 +40,57 @@ export const createResizeObserver = (): {
 		dimensions$: observedElement$,
 
 		/** Directive to be attached to html element in order to listen to resize events */
+		directive,
+	};
+};
+
+/**
+ * Create a resize observer that can be applied to multiple elements
+ * @returns An object containing the store with the dimentions map of observed elements (ResizeObserverEntry), the directive to be applied to the html element to be observed
+ */
+export const createResizeObserverArray = (): {
+	dimensionsMap$: ReadableSignal<Map<HTMLElement, ResizeObserverEntry>>;
+	directive: Directive<void, SSRHTMLElement>;
+} => {
+	const {elements$, directive} = createBrowserStoreArrayDirective();
+
+	const observedElements$ = derived<Map<HTMLElement, ResizeObserverEntry>, ReadableSignal<HTMLElement[]>>(
+		elements$,
+		(elements, set) => {
+			const dimensionsMap = new Map<HTMLElement, ResizeObserverEntry>();
+
+			if (elements.length === 0) {
+				set(dimensionsMap);
+				return noop;
+			}
+
+			const observer = new ResizeObserver((entries) => {
+				for (const entry of entries) {
+					dimensionsMap.set(entry.target as HTMLElement, entry);
+				}
+				set(new Map(dimensionsMap)); // create new Map to trigger reactivity
+			});
+
+			for (const element of elements) {
+				observer.observe(element);
+			}
+
+			return () => {
+				observer.disconnect();
+				dimensionsMap.clear();
+			};
+		},
+		new Map(),
+	);
+
+	return {
+		/**
+		 * Store which contains a Map of observed elements to their dimensions (ResizeObserverEntry)
+		 * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserverEntry | MDN documentation}
+		 */
+		dimensionsMap$: observedElements$,
+
+		/** Directive to be attached to html elements in order to listen to resize events */
 		directive,
 	};
 };
